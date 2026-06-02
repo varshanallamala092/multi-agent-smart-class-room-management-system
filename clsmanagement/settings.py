afdcaa8a8d17ea3c3a21e8e55683816cf0981514
class <class@gmail.com>
2026-06-02 11:36:06 +0530

    added Procfile for deployment

--- clsmanagement/settings.py
+++ clsmanagement/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-h)!jh-nc0ah&r4tzy9t3r@qg&+i0=ulixn0_16z$^8x7qx7-r2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -128,6 +128,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
